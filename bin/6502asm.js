291d8b95f6fcb73ba2d26ac56205e45f8562de17
Damián Silvani <munshkr@gmail.com>
2016-08-05 23:03:19 -0300

    Define and export an Assembler class

--- bin/6502asm.js
+++ bin/6502asm.js
@@ -8,8 +8,8 @@ program
   .usage('[options] <file>')
   .option('-o, --output [file]',
     `output object file (default: ${DEFAULT_OUT})`, DEFAULT_OUT)
-  .option('-a, --print-ast', 'print parser\'s AST')
-  .option('-s, --print-symbols', 'print symbols table')
+  .option('-s, --symbols [file]', 'output symbols table (in JSON)')
+  .option('-d, --debug', 'print debugging info')
   .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
@@ -27,10 +27,9 @@ if (program.args.length > 1) {
   process.exit(1);
 }
 
-const PEG = require('pegjs');
+
 const fs = require('fs');
-const path = require('path');
-const assemble = require('../lib/assembler.js').assemble;
+const Assembler = require('../lib/assembler.js').Assembler;
 
 function abort(err) {
   if (err.location) {
@@ -42,46 +41,18 @@ function abort(err) {
   process.exit(1);
 }
 
-function generateParser(grammarPath, options) {
-  const grammar = fs.readFileSync(grammarPath).toString();
-  return PEG.buildParser(grammar, options);
-}
-
-function parse(sourcePath, options) {
-  let parser = generateParser(path.join(__dirname, '../lib/grammar.peg'));
-
-  const source = fs.readFileSync(sourcePath).toString();
-  const ast = parser.parse(source);
-
-  if (options.printAst) {
-    console.log("=== AST ===");
-    console.dir(ast, {colors: true, depth: null});
-  }
-
-  return ast;
-}
-
-function assembleAST(ast, outputPath, options) {
-  const out = assemble(ast, { debug: true });
-  const buf = Buffer.from(out.objectCode);
-
-  fs.writeFileSync(outputPath, buf);
-
-  if (options.printSymbols) {
-    console.log("=== Symbol table ===");
-    for (let symbol in out.symbolTable) {
-      console.log(`$${out.symbolTable[symbol].toString(16, 4)}: ${symbol}`);
-    }
-  }
-}
-
-
-const inputPath = program.args[0];
-const outputPath = program.output;
-
 try {
-  let ast = parse(inputPath, { printAst: program.printAst });
-  assembleAST(ast, outputPath, { printSymbols: program.printSymbols });
+  let assembler = new Assembler({ debug: program.debug });
+
+  let source = fs.readFileSync(program.args[0]).toString();
+  let {objectCode, symbolTable} = assembler.assemble(source, { debug: true });
+
+  let buf = Buffer.from(objectCode);
+  fs.writeFileSync(program.output, buf);
+
+  if (program.symbols) {
+    fs.writeFileSync(program.symbols, JSON.stringify(symbolTable, null, 4));
+  }
 } catch (err) {
   abort(err);
 }
--- test/assembler.js
+++ test/assembler.js
@@ -1,132 +1,131 @@
 const fs = require('fs');
 const assert = require('assert');
-const PEG = require('pegjs');
 const path = require('path');
 
-const grammar = fs.readFileSync(path.join(__dirname, '../lib/grammar.peg')).toString();
-const parse = PEG.buildParser(grammar).parse;
-const assemble = require('../lib/assembler').assemble;
+let Assembler = require('../lib/assembler').Assembler;
+
+let asm = new Assembler();
 
 describe('6502 Assembler', () => {
   describe('Labels', () => {
     it('registers a label on the symbol table', () => {
-      let prg = parse(`
+      let prg = `
         foo:
         bar:
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [],
         symbolTable: { foo: 0, bar: 0 }
       });
     });
 
     it('registers a label on the symbol table (2)', () => {
-      let prg = parse(`
+      let prg = `
         foo: nop
              nop
         bar: nop
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0xea, 0xea, 0xea],
         symbolTable: { foo: 0, bar: 2 }
       });
     });
 
     it('throws an error when label is already defined', () => {
-      let prg = parse(`
+      let prg = `
         foo: nop
         foo: nop
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('registers a label definition on the symbol table', () => {
-      let prg = parse(`
+      let prg = `
         foo = $8001
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [],
         symbolTable: { foo: 0x8001 }
       });
     });
 
     it('updates program counter when redefining \'*\' label', () => {
-      let prg = parse(`
+      let prg = `
         * = 0x801
         basic:
         * = 0xc000
         code:
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [],
         symbolTable: { basic: 0x801, code: 0xc000 }
       });
     });
 
     it('throws an error when label is already defined (w/label def)', () => {
-      let prg = parse(`
+      let prg = `
         foo:
           foo = 1
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('throws an error when label is already defined ' +
        '(w/label def) (2)', () => {
 
-      let prg = parse(`
+      let prg = `
         foo = 1
         foo = 2
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
   });
 
   describe('Directives', () => {
     it('inserts bytes with .byte directive', () => {
-      let prg = parse(`
+      let prg = `
         data:
           .byte 1, 2, 3, 0, 255
         code:
           nop
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [1, 2, 3, 0, 255, 0xea],
         symbolTable: { data: 0, code: 5 }
       });
     });
 
     it('inserts words with .word directive', () => {
-      let prg = parse(`
+      let prg = `
         data:
           .word $20, 0, $1000, $fffe
         code:
           nop
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0x20, 0, 0, 0, 0, 0x10, 0xfe, 0xff, 0xea],
         symbolTable: { data: 0, code: 8 }
       });
     });
 
     it('inserts bytes with .aasc directive', () => {
-      let prg = parse(`
+      let prg = `
         data:
           .byte "hola", "chau"
         code:
           nop
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0x68, 0x6f, 0x6c, 0x61,
                      0x63, 0x68, 0x61, 0x75,
                      0xea],
@@ -137,30 +136,30 @@ describe('6502 Assembler', () => {
 
   describe('Instructions', () => {
     it('throws an error when using an unknown mnemonic', () => {
-      let prg = parse(`
+      let prg = `
         wat
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('throws an error when using an invalid addressing mode', () => {
-      let prg = parse(`
+      let prg = `
         pla $1000
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('increments PC after instructions of different sizes', () => {
-      let prg = parse(`
+      let prg = `
         a: lda $1000    ; 3 bytes
         b: lda $42, x   ; 2 bytes
         c: nop          ; 1 byte
         d: bne a        ; 2 bytes
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0xad, 0, 0x10,
                      0xb5, 0x42,
                      0xea,
@@ -170,22 +169,22 @@ describe('6502 Assembler', () => {
     });
 
     it('throws an error when referring to an undefined label', () => {
-      let prg = parse(`
+      let prg = `
         jmp wtf
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('uses relative addresses to labels on branch instructions', () => {
-      let prg = parse(`
+      let prg = `
         work: rts
         loop: jsr work
               dex
               bne loop
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0x60,
                      0x20, 0, 0,
                      0xca,
@@ -195,37 +194,37 @@ describe('6502 Assembler', () => {
     });
 
     it('throws an error when relative addresses is out of range', () => {
-      let prg = parse(`
+      let prg = `
         init: nop
         * = $200
               bne init
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('refers * to PC, the location of current instruction', () => {
-      let prg = parse(`
+      let prg = `
               nop
               nop
         loop: jmp *
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0xea, 0xea, 0x4c, 2, 0],
         symbolTable: { loop: 2 }
       });
     });
 
     it('negative values are stored in two\'s complement', () => {
-      let prg = parse(`
+      let prg = `
         foo = -1
         bar = -25
         lda #foo
         lda #bar
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0xa9, 0xff,
                      0xa9, 0xe7],
         symbolTable: { foo: -1, bar: -25 }
@@ -235,46 +234,46 @@ describe('6502 Assembler', () => {
     it('throws an error if instruction in Immediate mode has a label ' +
        'operand that refers to a 16-bit number', () => {
 
-      let prg = parse(`
+      let prg = `
         foo = $200
         adc #foo
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('throws an error if instruction in Indirect X-Indexed mode has ' +
        'a label operand that refers to a 16-bit number', () => {
 
-      let prg = parse(`
+      let prg = `
         init = $200
         adc (init, x)
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
 
     it('throws an error if instruction in Indirect Y-Indexed mode has ' +
        'a label operand that refers to a 16-bit number', () => {
 
-      let prg = parse(`
+      let prg = `
         foo = $200
         adc (foo), y
-      `);
+      `;
 
-      assert.throws(() => { assemble(prg); }, Error);
+      assert.throws(() => { asm.assemble(prg); }, Error);
     });
   });
 
   describe('Expressions', () => {
     it('with < or > evaluate to LSB/MSB of term', () => {
-      let prg = parse(`
+      let prg = `
         loadAddr = $1040
         lda <loadAddr
         lda >loadAddr
-      `);
+      `;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [0xa5, 0x40,
                      0xa5, 0x10],
         symbolTable: { loadAddr: 0x1040 }
@@ -282,144 +281,144 @@ describe('6502 Assembler', () => {
     });
 
     it('can use "-" unary operator', () => {
-      let prg = parse(`foo = -1337`);
+      let prg = `foo = -1337`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: -1337 }
       });
     });
 
     it('can use "~" unary operator', () => {
-      let prg = parse(`foo = ~0b11001100`);
+      let prg = `foo = ~0b11001100`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: -205 }
       });
     });
 
     it('can use "!" unary operator', () => {
-      let prg = parse(`isTrue = !0
-                       isFalse = !21`);
+      let prg = `isTrue = !0
+                       isFalse = !21`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
     it('can use "+" binary operator', () => {
-      let prg = parse(`foo = 10 + 0x100`);
+      let prg = `foo = 10 + 0x100`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 0x10a }
       });
     });
 
     it('can use "-" binary operator', () => {
-      let prg = parse(`* = $100
-                       foo = $200 - *`);
+      let prg = `* = $100
+                       foo = $200 - *`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 0x100 }
       });
     });
 
     it('can use "*" binary operator', () => {
-      let prg = parse(`foo = 32 * 2`);
+      let prg = `foo = 32 * 2`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 64 }
       });
     });
 
     it('can use "/" binary operator', () => {
-      let prg = parse(`foo = 100 / 2`);
+      let prg = `foo = 100 / 2`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 50 }
       });
     });
 
     it('can use ">>" binary operator', () => {
-      let prg = parse(`foo = 64 >> 3`);
+      let prg = `foo = 64 >> 3`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 8 }
       });
     });
 
     it('can use "<<" binary operator', () => {
-      let prg = parse(`foo = 2 << 4`);
+      let prg = `foo = 2 << 4`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 32 }
       });
     });
 
     it('can use "<=", ">=" binary operators', () => {
-      let prg = parse(`isTrue  = 4 <= 4
-                       isFalse = 1 >= 2`);
+      let prg = `isTrue  = 4 <= 4
+                       isFalse = 1 >= 2`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
 
     it('can use "<", ">" binary operators', () => {
-      let prg = parse(`isTrue  = 2 < 4
-                       isFalse = 10 > 15`);
+      let prg = `isTrue  = 2 < 4
+                       isFalse = 10 > 15`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
 
     it('can use "==", "!=" binary operators', () => {
-      let prg = parse(`isTrue  = (42 == 42)
-                       isFalse = (2+2 == 5)`);
+      let prg = `isTrue  = (42 == 42)
+                       isFalse = (2+2 == 5)`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
 
     it('can use "&" binary operators', () => {
-      let prg = parse(`foo = $1234
-                       bar = foo & $ff`);
+      let prg = `foo = $1234
+                       bar = foo & $ff`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 0x1234, bar: 0x34 }
       });
     });
 
     it('can use "^" binary operators', () => {
-      let prg = parse(`foo = 1 ^ $ff`);
+      let prg = `foo = 1 ^ $ff`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 0xfe }
       });
     });
 
     it('can use "|" binary operators', () => {
-      let prg = parse(`foo = 1 | 4 | 8`);
+      let prg = `foo = 1 | 4 | 8`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { foo: 13 }
       });
     });
 
     it('can use "&&" binary operators', () => {
-      let prg = parse(`isTrue = 42 && 1
-                       isFalse = 0 && 1`);
+      let prg = `isTrue = 42 && 1
+                       isFalse = 0 && 1`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
 
     it('can use "||" binary operators', () => {
-      let prg = parse(`isTrue = 0 || 1
-                       isFalse = 0 || 0`);
+      let prg = `isTrue = 0 || 1
+                       isFalse = 0 || 0`;
 
-      assert.deepEqual(assemble(prg), {
+      assert.deepEqual(asm.assemble(prg), {
         objectCode: [], symbolTable: { isTrue: 1, isFalse: 0 }
       });
     });
@@ -428,45 +427,45 @@ describe('6502 Assembler', () => {
   it('throws an error if using .byte with a label that refers to a 16-bit ' +
      'number', () => {
 
-    let prg = parse(`
+    let prg = `
       foo = $200
       .byte foo
-    `);
+    `;
 
-    assert.throws(() => { assemble(prg); }, Error);
+    assert.throws(() => { asm.assemble(prg); }, Error);
   });
 
   it('throws an error if a .byte datum refers to an unresolved symbol', () => {
-    let prg = parse(`
+    let prg = `
       .byte foo
       foo = $200
-    `);
+    `;
 
-    assert.throws(() => { assemble(prg); }, Error);
+    assert.throws(() => { asm.assemble(prg); }, Error);
   });
 
   it('throws an error if a label refers to an unresolved symbol', () => {
-    let prg = parse(`
+    let prg = `
       bar = foo + 0x10
       foo = $200
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [],
       symbolTable: { bar: 0x210, foo: 0x200 }
     });
   });
 
   it('optimizes and uses zero-page addressing mode if possible', () => {
-    let prg = parse(`
+    let prg = `
       lda $1000
       lda $10
 
       lda $1000, x
       lda $10, x
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [0xad, 0, 0x10,
                    0xa5, 0x10,
                    0xbd, 0, 0x10,
@@ -478,29 +477,29 @@ describe('6502 Assembler', () => {
   it('optimizes and uses zero-page addressing mode if possible ' +
      '(labels)', () => {
 
-    let prg = parse(`
+    let prg = `
       a = $fe
       tmp = a
       lda tmp
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [0xa5, 0xfe],
       symbolTable: { a: 0xfe, tmp: 0xfe }
     });
   });
 
   it('compiles instructions with unresolved references as operands', () => {
-    let prg = parse(`
+    let prg = `
       start:
         jsr play
         rts
 
       play:
         rts
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [0x20, 0x4, 0,
                    0x60,
                    0x60],
@@ -511,28 +510,28 @@ describe('6502 Assembler', () => {
   it('fails to optimize with zero-page mode if operand has an ' +
      'unresolved symbol', () => {
 
-    let prg = parse(`
+    let prg = `
       lda hmm
       hmm = $fe
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [0xad, 0xfe, 0],
       symbolTable: { hmm: 0xfe }
     });
   });
 
   it('resolved chain of label dependences with multiple passes', () => {
-    let prg = parse(`
+    let prg = `
       * = a
       * = b
         a = *
       * = c
         b = *
       c = $1000
-    `);
+    `;
 
-    assert.deepEqual(assemble(prg), {
+    assert.deepEqual(asm.assemble(prg), {
       objectCode: [],
       symbolTable: { a: 0x1000, b: 0x1000, c: 0x1000 }
     });
